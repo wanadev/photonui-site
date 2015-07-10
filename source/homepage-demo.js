1a239c3d3a46a0b1a16be36bede9074652a46dd8
Fabien LOISON <flo@flogisoft.com>
2015-07-10 17:12:22 +0200

    homepage window width -> 300px

--- source/homepage-demo.js
+++ source/homepage-demo.js
@@ -3,6 +3,7 @@ var pos = photonui.Helpers.getAbsolutePosition("demo");
 var win = new photonui.Window({
     title: "Hello Photon",
     x: pos.x, y: pos.y,
+    width: 300,
     visible: true,
     padding: 10,
     child: new photonui.Label({
