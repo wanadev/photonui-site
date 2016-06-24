30c14583a0b6c9715455e3d56e7fc2f763912152
Fabien LOISON <flo@flogisoft.com>
2016-06-24 11:30:00 +0200

    demo editor now use codemirror. PhotonUI Updated

--- themes/photonui/source/js/script-runner.js
+++ themes/photonui/source/js/script-runner.js
@@ -2,9 +2,11 @@
     var e_code = document.getElementById("demo-code");
     var e_error = document.getElementById("demo-error");
     var e_btn = document.getElementById("run-script");
+    var demoJs = document.getElementById("demo-code-content").textContent;
+    var editor;
 
     function runScript() {
-        var script = e_code.textContent;
+        var script = editor.getValue();
 
         // Hide error banner
         e_error.style.display = "none";
@@ -18,7 +20,7 @@
             widget = photonui.getWidget(tags[i].id);
             if (widget) widgets.push(widget);
         }
-        while (widget = widgets.pop()) {
+        while (widget = widgets.pop()) {  // jshint ignore:line
             try {
                 widget.destroy();
             }
@@ -30,18 +32,13 @@
 
         // Run the code
         try {
-            var demoFunction = new Function(script);
+            var demoFunction = new Function(script);  // jshint ignore:line
             demoFunction();
         }
         catch (error) {
             e_error.textContent = error;
             e_error.style.display = "block";
         }
-    }
-
-    function highlight() {
-        hljs.highlightBlock(e_code);
-        e_code.className = "";  // Remove styles applied to the <pre>
     }
 
     function addRunButton() {
@@ -66,7 +63,7 @@
             btn.onclick = function() {
                 scrollTo(0, photonui.Helpers.getAbsolutePosition("content").y);
                 this.innerHTML = this.innerHTML.replace(/<br(\s*\/)?>(<\/br>)?/ig, "\n");
-                e_code.textContent = this.textContent;
+                editor.setValue(this.textContent);
                 highlight();
                 runScript();
             }.bind(codes[i]);
@@ -75,37 +72,34 @@
         }
     }
 
+    function mkEditor() {
+        editor = new CodeMirror(function(elt) {
+            e_code.appendChild(elt);
+        }, {
+            value: demoJs,
+            mode: "javascript",
+            theme: "tomorrow-night-bright",
+            smartIndent: true,
+            indentUnit: 4,
+            tabSize: 4,
+            indentWithTabs: false,
+            //keyMap: "vim",
+            lineNumbers: false,
+            inputStyle: "contenteditable",
+            scrollbarStyle: "null",
+            extraKeys: {
+                "Ctrl-Enter": function() {
+                    runScript();
+                }
+            }
+        });
+    }
+
     e_btn.onclick = runScript;
 
-    e_code.onblur = function() {
-        e_code.innerHTML = e_code.innerHTML.replace(/<br(\s*\/)?>(<\/br>)?/ig, "\n");
-        highlight();
-    }
-
-    e_code.onfocus = function() {
-        // Remove highlight
-        var tags = document.querySelectorAll("#demo-code *");
-        for (var i=0 ; i<tags.length ; i++) {
-            tags[i].className = "";
-        }
-    }
-
-    e_code.onkeydown = function(event) {
-        // Ctrl + Enter -> RUN
-        if (event.ctrlKey && event.keyCode == 13) {
-            runScript();
-            event.preventDefault();
-        }
-        // Tab -> insert spaces
-        else if (event.keyCode == 9) {
-            document.execCommand("insertText", false, "    ");
-            event.preventDefault();
-        }
-    }
-
     window.addEventListener("load", function() {
+        mkEditor();
         runScript();
-        highlight();
         addRunButton();
     });
 
